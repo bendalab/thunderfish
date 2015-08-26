94ab1ceaee9c4c8d2945f9b59f79ae17a0f8f8f1
Juan Sehuanes <jfsehuanes@gmail.com>
2015-08-26 12:41:43 +0200

    in npy_to_hist.py the plot showing the distribution of eod-frequencies was edited. still needs minor cosmetics.

--- sorting_tools/npy_to_hist.py
+++ sorting_tools/npy_to_hist.py
@@ -1,48 +1,61 @@
 __author__ = 'raab'
 import sys
+import glob
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython import embed
 
-def load_npy_to_list():
+def load_data(glob_ls):
     """
-    loads an .npy data (numpy.array) and converts it to a list.
+    loads all .npy data (numpy.array) and converts it to a dictionary using part from filenames as keys.
 
+    :param glob_ls: list of .npy files
     :return: data (list of frequencies)
     """
     print 'loading data ...'
-    data = np.load('%s' %sys.argv[1])
-    data = data.tolist()
+    # load data as a dictionary using either pulsefish or wavefish as key and the np.array as value.
+    data = {curr_file.split('_')[-1].split('.npy')[0]: np.load(curr_file) for curr_file in glob_ls}
     print 'data loaded successfully'
     return data
 
-def creat_histo(data):
+def create_histo(data):
     """
     gets a list of data and creates an histogram of this data.
 
-    :param data:
+    :param data: dictionary with fishtype as keys and np.array with EOD-Frequencies as values.
     """
     print 'creating histogramm ...'
-    hist, bins = np.histogram(data, bins= len(data)//4)
-    width = 0.7 * (bins[1] - bins[0])
-    center = (bins[:-1] + bins[1:]) / 2
 
-    fig, ax = plt.subplots(facecolor='white')
-    ax.bar(center, hist, align='center', width=width)
-    ax.spines["right"].set_visible(False)
-    ax.spines["top"].set_visible(False)
-    ax.tick_params(axis='both', direction='out')
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
-    ax.set_ylabel('counts')
-    ax.set_xlabel('frequency')
-    plt.xticks(np.arange(0, max(data)+100, 250))
-    plt.title('Histogram')
-    plt.show()
+    inch_factor = 2.54
+    sns.set_context("poster")
+    sns.axes_style('white')
+    sns.set_style("ticks")
+    fig, ax = plt.subplots(figsize=(20./inch_factor, 15./inch_factor))
+    colors = ['salmon', 'cornflowerblue']
+
+    for enu, curr_fishtype in enumerate(data.keys()):
+        hist, bins = np.histogram(data[curr_fishtype], bins=len(data[curr_fishtype])//4)
+        width = 0.7 * (bins[1] - bins[0])
+        center = (bins[:-1] + bins[1:]) / 2
+        ax.bar(center, hist, align='center', width=width, alpha=0.8, facecolor=colors[enu], label=curr_fishtype)
+
+    ax.set_ylabel('Counts', fontsize=16)
+    ax.set_xlabel('Frequency [Hz]', fontsize=16)
+    ax.set_xticks(np.arange(0, max(np.hstack(data.values()))+100, 250))
+    ax.tick_params(axis='both', which='major')
+    ax.set_title('Distribution of EOD-Frequencies')
+    ax.legend(frameon=False, loc='best', fontsize=14)
+    sns.despine(fig=fig, ax=ax, offset=10)
+    fig.tight_layout()
+    fig.savefig('histo_of_eod_freqs.pdf')
+    plt.close()
 
 def main():
-    data = load_npy_to_list()
+    npy_files = glob.glob('*.npy')
+    data = load_data(npy_files)
 
-    creat_histo(data)
+    create_histo(data)
 
     print 'code finished'
 
