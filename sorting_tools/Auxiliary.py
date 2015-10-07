dde58d9d825be6fb2f7476c07f901af1e68c226a
Juan Sehuanes <jfsehuanes@gmail.com>
2015-10-07 12:43:40 +0200

    FishRecording class added and proper functions were added to Auxiliary.py

--- sorting_tools/Auxiliary.py
+++ sorting_tools/Auxiliary.py
@@ -1,9 +1,12 @@
 __author__ = 'juan'
 # Imports
+import os
 import numpy as np
+import wave
 import matplotlib.pyplot as plt
 import sys
 import seaborn as sns
+from scipy.signal import butter, filtfilt
 from IPython import embed
 
 def peakdet(v, delta, x=None):
@@ -122,6 +125,47 @@ def plot_dfs_histogram(dfs_array, binwidth='FD'):
     fig.tight_layout()
     fig.savefig('figures/histo_of_dfs.pdf')
     plt.close()
+
+def butter_lowpass(highcut, fs, order=5):
+    nyq = 0.5 * fs
+    high = highcut / nyq
+    b, a = butter(order, high, btype='low')
+    return b, a
+
+
+def butter_lowpass_filter(data, highcut, fs, order=5):
+    b, a = butter_lowpass(highcut, fs, order=order)
+    y = filtfilt(b, a, data)
+    return y
+
+def load_trace(modfile):
+    """ Loads modified .wav and returns a two dimensional np array with the Time trace and corresponding Amplitude.
+    """
+    recording = wave.open(modfile)
+    sample_rate = recording.getframerate()
+    frames_to_read = 30 * sample_rate  # read not more than the first 30 seconds
+    data = np.fromstring(recording.readframes(frames_to_read), np.int16).astype(float)  # read data
+    data -= np.mean(data)
+    time_length = float(data.size) / sample_rate
+    t_trace = np.linspace(0.0, time_length, num=data.size)
+    return t_trace, data, sample_rate
+
+
+def conv_to_single_ch_audio(audiofile):
+    """ This function uses the software avconv to convert the current file to a single channel audio wav-file
+    (or mp3-file), with which we can work afterwards using the package wave. Returns the name of the modified
+    file as a string
+
+    :rtype : str
+    :param audiofile: sound-file that was recorded
+    """
+
+    base, ext = os.path.splitext(audiofile)
+    base = base.split('/')[-1]
+    new_mod_filename = 'recording_' + base + '_mod.wav'
+    os.system('avconv -i {0:s} -ac 1 -y -acodec pcm_s16le {1:s}'.format(audiofile, new_mod_filename))
+    return new_mod_filename
+
 
 
 if __name__ == '__main__':
