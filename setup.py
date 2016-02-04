42a661c73d15ab8b3e959921fb62a67c2ec19ae1
Fabian Sinz <sinz@bcm.edu>
2016-02-04 14:41:32 -0600

    try different variations of travis.yml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='thunderfish',
       version='0.5',
-      packages=['thunderfish'],
+      packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
       description='Python scripts for analyzing recordings of e-fish electric fields.',
       author='Juan F. Sehuanes, Till Raab, Fabian Sinz, Jan Benda, Joerg Henninger, Jan Grewe',
       requires=['matplotlib', 'numpy', 'scipy', 'seaborn', 'IPython', 'audioread']
