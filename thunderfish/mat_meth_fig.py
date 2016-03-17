5e570f2d7e29bbfe948e49b7b7efa9a601704f6e
Till <till.raab@student.uni-tuebingen.de>
2016-03-17 10:45:41 +0100

    changes in the ploted data

--- thunderfish/mat_meth_fig.py
+++ thunderfish/mat_meth_fig.py
@@ -10,92 +10,95 @@ import scipy.stats as sps
 def load_data():
     wave_psd_prop = np.load('wave_PSD_algor.npy')
     pulse_psd_prop = np.load('pulse_PSD_algor.npy')
+    multi_wave_psd_prop = np.load('multi_wave_PSD_algor.npy')
 
-    wave_p2v_prop = np.load('wave_p2v_prop.npy')
-    pulse_p2v_prop = np.load('pulse_p2v_prop.npy')
+    wave_p2v_prop = np.load('wave_p2v_algor.npy')
+    pulse_p2v_prop = np.load('pulse_p2v_algor.npy')
+    multi_wave_p2v_prop = np.load('multi_wave_p2v_algor.npy')
 
-    wave_skewness = np.load('wave_skewness.npy')
-    pulse_skewness = np.load('pulse_skewness.npy')
+    wave_hist_algor = np.load('wave_hist_algor.npy')
+    pulse_hist_algor = np.load('pulse_hist_algor.npy')
+    multi_wave_hist_algor = np.load('multi_wave_hist_algor.npy')
 
-    # embed()
-    # quit()
+    mixed_psd_prop = np.load('mixed_PSD_algor.npy')
+    mixed_p2v_prop = np.load('mixed_p2v_algor.npy')
+    mixed_hist_algor = np.load('mixed_hist_algor.npy')
 
-    return wave_psd_prop, pulse_psd_prop, wave_p2v_prop, pulse_p2v_prop, wave_skewness, pulse_skewness
+    return wave_psd_prop, pulse_psd_prop, multi_wave_psd_prop, wave_p2v_prop, pulse_p2v_prop, multi_wave_p2v_prop,\
+           wave_hist_algor, pulse_hist_algor, multi_wave_hist_algor, mixed_psd_prop, mixed_p2v_prop, mixed_hist_algor
 
 
-def plot_data(wave_prop, pulse_prop, wave_p2v_prop, pulse_p2v_prop):
-    # make boxplots !!!
-    # lieber punktewolke !!!
+def plot_data(wave_psd_prop, pulse_psd_prop, multi_wave_psd_prop, wave_p2v_prop, pulse_p2v_prop,
+              multi_wave_p2v_prop, wave_hist_algor, pulse_hist_algor, multi_wave_hist_algor, mixed_psd_prop,
+              mixed_p2v_prop, mixed_hist_algor):
+
     sns.set_context("poster")
     sns.axes_style('white')
     # sns.set_style("ticks")
 
-    fig, ax = plt.subplots()
-    ax.plot(wave_prop, wave_p2v_prop, 'o', color = 'red', label='wave-type', alpha= 0.7, markersize= 5)
-    ax.plot(pulse_prop, pulse_p2v_prop, 'o', color= 'blue', label= 'pulse-type', alpha= 0.7, markersize= 5)
+    fig1, ax = plt.subplots()
+    ax.plot(wave_psd_prop, wave_p2v_prop, 'o', color = 'blue', label='wave-type', alpha= 0.7, markersize= 5)
+    # ax.plot(pulse_psd_prop, pulse_p2v_prop, 'o', color= 'red', label= 'pulse-type', alpha= 0.7, markersize= 5)
+    ax.plot(multi_wave_psd_prop, multi_wave_p2v_prop, 'o', color = 'green', label='multi wave-type', alpha= 0.7, markersize= 5)
+    ax.plot(pulse_psd_prop, pulse_p2v_prop, 'o', color= 'red', label= 'pulse-type', alpha= 0.7, markersize= 5)
+    # ax.plot(mixed_psd_prop, mixed_p2v_prop, 'o', color = 'k', label='wave-type', alpha= 1, markersize= 6)
+
     plt.legend()
-    # ax.tick_params(axis='both', which='major', labelsize=14)
     ax.set_xlabel('psd_algo', fontsize=14)
-    ax.set_ylabel('soundtrace_algo', fontsize=14)
-    plt.ylim([-0.1, 1.1])
-    # fig.tight_layout()
+    ax.set_ylabel('p2v_algo', fontsize=14)
+    ax.set_title('psd_algo vs. p2v_algo', fontsize=14)
+
+    fig1.tight_layout()
     plt.draw()
 
-    #####################################
-    fig, ax = plt.subplots()
-    ax.plot([1+random.uniform(-0.1, 0.1) for i in np.arange(len(wave_prop))], wave_prop, '.', color = 'red', label='wave-type', alpha= 0.7)
-    ax.plot([1+random.uniform(-0.1, 0.1) for i in np.arange(len(pulse_prop))], pulse_prop, '.', color = 'blue', label='pulse-type', alpha = 0.7)
+    fig2, ax2 = plt.subplots()
+    ax2.plot(wave_psd_prop, wave_hist_algor, 'o', color = 'blue', label='wave-type', alpha= 0.7, markersize=5)
+    ax2.plot(multi_wave_psd_prop, multi_wave_hist_algor, 'o', color = 'green', label='multi wave-type', alpha= 0.7, markersize=5)
+    ax2.plot(pulse_psd_prop, pulse_hist_algor, 'o', color = 'red', label='pulse-type', alpha= 0.7, markersize=5)
+    # ax2.plot(multi_wave_psd_prop, multi_wave_hist_algor, 'o', color = 'green', label='multi wave-type', alpha= 0.7, markersize=5)
+    # ax2.plot(mixed_psd_prop, mixed_hist_algor, 'o', color = 'k', label='wave-type', alpha= 1, markersize=6)
 
     plt.legend()
-    ax.set_xticks([1, 2])
-
-    ax.set_ylabel('psd_prop', color='k')
-    for tl in ax.get_yticklabels():
-        tl.set_color('k')
-
-    ax2 = ax.twinx()
-    ax2.plot([2+random.uniform(-0.1, 0.1) for i in np.arange(len(wave_p2v_prop))], wave_p2v_prop, '.', color = 'red', alpha= 0.7)
-    ax2.plot([2+random.uniform(-0.1, 0.1) for i in np.arange(len(pulse_p2v_prop))], pulse_p2v_prop, '.', color = 'blue', alpha= 0.7)
-    ax2.set_ylabel('soundtrace_prop', color='k')
-    for tl in ax2.get_yticklabels():
-        tl.set_color('k')
-
-    ax.set_xticklabels(['psd_algo', 'soundtrace_algo'])
-    plt.xlim([0.5, 2.5])
-    plt.ylim([-0.1, 1.1])
-    plt.tight_layout()
+    ax2.set_xlabel('psd_algo', fontsize=14)
+    ax2.set_ylabel('hist_algo', fontsize=14)
+    ax2.set_title('psd_algo vs. hist_algo')
+    fig2.tight_layout()
     plt.draw()
 
+    fig3, ax3 = plt.subplots()
+    ax3.plot(wave_p2v_prop, wave_hist_algor, 'o', color = 'blue', label='wave-type', alpha= 0.7, markersize=5)
+    ax3.plot(multi_wave_p2v_prop, multi_wave_hist_algor, 'o', color = 'green', label='multi wave-type', alpha= 0.7, markersize=5)
+    ax3.plot(pulse_p2v_prop, pulse_hist_algor, 'o', color = 'red', label='pulse-type', alpha= 0.7, markersize=5)
+    # ax3.plot(multi_wave_p2v_prop, multi_wave_hist_algor, 'o', color = 'green', label='multi wave-type', alpha= 0.7, markersize=5)
+    # ax3.plot(mixed_p2v_prop, mixed_hist_algor, 'o', color = 'k', label='wave-type', alpha= 1, markersize=6)
 
-def plot_psd_algo(wave_prop, pulse_prop, wave_p2v_prop, pulse_p2v_prop, wave_skewness, pulse_skewness):
+    plt.legend()
+    ax3.set_xlabel('p2v_algo', fontsize=14)
+    ax3.set_ylabel('hist_algo', fontsize=14)
+    ax3.set_title('p2v_algo vs. hist_algo')
+    fig3.tight_layout()
+    plt.draw()
+
+def plot_psd_algo(wave_prop, pulse_prop, multi_wave_prop):
     inch_factor = 2.54
     sns.set_context("poster")
     sns.axes_style('white')
     # sns.set_style("ticks")
 
-    fig2= plt.figure(figsize=(45./ inch_factor, 20./ inch_factor))
-    ax1 = fig2.add_subplot(2, 4, (1, 5))
-    dafr = pd.DataFrame([wave_prop, pulse_prop]) #turn
+    fig2= plt.figure(figsize=(35./ inch_factor, 20./ inch_factor))
+    ax1 = fig2.add_subplot(2, 3, (1, 4))
+    dafr = pd.DataFrame([wave_prop, multi_wave_prop, pulse_prop]) #turn
     dafr = dafr.transpose()
-    dafr.columns = ['wave', 'pulse']
-    sns.violinplot(data=dafr,  ax=ax1)
+    dafr.columns = ['wave', 'multi-wave', 'pulse']
+    sns.violinplot(data=dafr,  ax=ax1, col=("blue", "green", "red"))
     ax1.set_ylabel('psd_proportion')
     ax1.set_xlabel('EOD-type')
     ax1.set_title('Fishsorting based on PSD')
 
-    ax4 = fig2.add_subplot(2, 4, (2, 6))
-    dafr2 = pd.DataFrame([wave_skewness, pulse_skewness])
-    dafr2 = dafr2.transpose()
-    dafr2.columns = ['wave', 'pulse']
-    sns.violinplot(data=dafr2, ax= ax4)
-    ax4.set_ylabel('skewness')
-    ax4.set_xlabel('PSD-type')
-    ax4.set_title('Fishsorting based on PSD')
-
     pulse_psd_data = np.load('pulse_psd_data.npy')
-    ax2 = fig2.add_subplot(2, 4, (3, 4))
-    ax2.axis([0, 3000, min(pulse_psd_data[1][:len(pulse_psd_data[0][pulse_psd_data[0]<3000])]), max(pulse_psd_data[1])+10])
-    ax2.plot(pulse_psd_data[0], pulse_psd_data[1], lw=2, color='green', alpha=0.7)
+    ax2 = fig2.add_subplot(2, 3, (2, 3))
+    ax2.axis([0, 1500, min(pulse_psd_data[1][:len(pulse_psd_data[0][pulse_psd_data[0]<1500])]), max(pulse_psd_data[1])+10])
+    ax2.plot(pulse_psd_data[0], pulse_psd_data[1], lw=2, color='red', alpha=0.7)
     # ax2.hist(pulse_psd_data[1][pulse_psd_data[0]<1000], 100)
     # print sps.kurtosis(pulse_psd_data[1][pulse_psd_data[0]<1000])
     ax2.set_xlabel('time [ms]')
@@ -103,8 +106,8 @@ def plot_psd_algo(wave_prop, pulse_prop, wave_p2v_prop, pulse_p2v_prop, wave_ske
     ax2.set_title('Pulsefish PSD')
 
     wave_psd_data = np.load('wave_psd_data.npy')
-    ax3 = fig2.add_subplot(2, 4, (7, 8))
-    ax3.axis([0, 3000, min(wave_psd_data[1][:len(wave_psd_data[0][wave_psd_data[0]<3000])]), max(wave_psd_data[1])+10])
+    ax3 = fig2.add_subplot(2, 3, (5, 6))
+    ax3.axis([0, 1500, min(wave_psd_data[1][:len(wave_psd_data[0][wave_psd_data[0]<1500])]), max(wave_psd_data[1])+10])
     ax3.plot(wave_psd_data[0], wave_psd_data[1], lw=2, color='blue', alpha=0.7)
     # ax3.hist(wave_psd_data[1][wave_psd_data[0]<1000], 100)
     # print sps.kurtosis(wave_psd_data[1][wave_psd_data[0]<1000])
@@ -112,14 +115,11 @@ def plot_psd_algo(wave_prop, pulse_prop, wave_p2v_prop, pulse_p2v_prop, wave_ske
     ax3.set_ylabel('Amplitude [a.u.]')
     ax3.set_title('Wavefish PSD')
 
-    # embed()
-    # quit()
-
     fig2.tight_layout()
     plt.draw()
 
 
-def plot_p2v_algo(wave_prop, pulse_prop, wave_p2v_prop, pulse_p2v_prop):
+def plot_p2v_algo(wave_p2v_prop, pulse_p2v_prop, multi_wave_p2v_prop):
     samplerate = 44100     ### really hard code
 
     inch_factor = 2.54
@@ -127,20 +127,20 @@ def plot_p2v_algo(wave_prop, pulse_prop, wave_p2v_prop, pulse_p2v_prop):
     sns.axes_style('white')
     # sns.set_style("ticks")
 
-    fig3= plt.figure(figsize=(45./ inch_factor, 20./ inch_factor))
+    fig3= plt.figure(figsize=(35./ inch_factor, 20./ inch_factor))
 
     ax1 = fig3.add_subplot(2, 3, (1, 4))
-    dafr2 = pd.DataFrame([wave_p2v_prop, pulse_p2v_prop])
+    dafr2 = pd.DataFrame([wave_p2v_prop, multi_wave_p2v_prop, pulse_p2v_prop])
     dafr2 =dafr2.transpose()
-    dafr2.columns = ['wave', 'pulse']
-    sns.violinplot(data=dafr2, ax=ax1)
+    dafr2.columns = ['wave', 'multi wave', 'pulse']
+    sns.violinplot(data=dafr2, ax=ax1, col=("blue", "green", "red"))
     ax1.set_ylabel('p2v_proportion')
     ax1.set_xlabel('EOD-type')
     ax1.set_title('Fishsorting based on soundtrace')
 
     pulse_trace_data = np.load('pulse_trace_data.npy')
     ax2 = fig3.add_subplot(2, 3, (2, 3))
-    ax2.plot(np.arange(3000) * 1.0 / samplerate * 1000, pulse_trace_data[:3000] / 2.0 ** 15, lw=2, color='green', alpha=0.7)
+    ax2.plot(np.arange(3000) * 1.0 / samplerate * 1000, pulse_trace_data[:3000] / 2.0 ** 15, lw=2, color='red', alpha=0.7)
     ax2.set_xlabel('time [ms]')
     ax2.set_ylabel('Amplitude [a.u.]')
     ax2.set_title('Pulsefish EODs')
@@ -156,14 +156,55 @@ def plot_p2v_algo(wave_prop, pulse_prop, wave_p2v_prop, pulse_p2v_prop):
     plt.draw()
 
 
+def plot_hist_algo(wave_hist_algor, pulse_hist_algor, multi_wave_hist_algor):
+    inch_factor = 2.54
+    sns.set_context("poster")
+    sns.axes_style('white')
+    # sns.set_style("ticks")
+
+    fig4= plt.figure(figsize=(35./ inch_factor, 20./ inch_factor))
+    ax1 = fig4.add_subplot(2, 3, (1, 4))
+    dafr = pd.DataFrame([wave_hist_algor, multi_wave_hist_algor, pulse_hist_algor]) #turn
+    dafr = dafr.transpose()
+    dafr.columns = ['wave', 'multi-wave', 'pulse']
+    sns.violinplot(data=dafr,  ax=ax1, col=("blue", "green", "red"))
+    ax1.set_ylabel('psd_proportion')
+    ax1.set_xlabel('EOD-type')
+    ax1.set_title('Fishsorting based on PSD')
+
+    wave_psd_data = np.load('wave_psd_data.npy')
+    wave_hist_data = wave_psd_data[1][:len(wave_psd_data[0][wave_psd_data[0]<1500])]
+    ax3 = fig4.add_subplot(2, 3, (2, 5))
+    n, bin, patch = ax3.hist(wave_hist_data, 50, color='blue', alpha=0.7, normed=True)
+    # ax3.set_ylim([0, max(n)+10])
+    ax3.set_ylabel('counts in histogram bin')
+    ax3.set_xlabel('amplitude of PSD')
+    ax3.set_title('Histogram of pulsefish PSD')
+
+    pulse_psd_data = np.load('pulse_psd_data.npy')
+    pulse_hist_data = pulse_psd_data[1][:len(pulse_psd_data[0][pulse_psd_data[0]<1500])]
+    ax2 = fig4.add_subplot(2, 3, (3, 6))
+    ax2.hist(pulse_hist_data, 50, color='red', alpha=0.7, normed=True)
+    # ax2.set_ylim([0, max(n)+10])
+    ax2.set_ylabel('counts in histogram bin')
+    ax2.set_xlabel('amplitude of PSD')
+    ax2.set_title('Histogram of pulsefish PSD')
+
+    fig4.tight_layout()
+
 def main():
-    wave_psd_prop, pulse_psd_prop, wave_p2v_prop, pulse_p2v_prop, wave_skewness, pulse_skewness= load_data()
+    wave_psd_prop, pulse_psd_prop, multi_wave_psd_prop, wave_p2v_prop, pulse_p2v_prop, multi_wave_p2v_prop, \
+    wave_hist_algor, pulse_hist_algor, multi_wave_hist_algor, mixed_psd_prop, mixed_p2v_prop, \
+    mixed_hist_algor= load_data()
 
-    # plot_data(wave_psd_prop, pulse_psd_prop, wave_p2v_prop, pulse_p2v_prop)
+    plot_data(wave_psd_prop, pulse_psd_prop, multi_wave_psd_prop, wave_p2v_prop, pulse_p2v_prop, multi_wave_p2v_prop,
+              wave_hist_algor, pulse_hist_algor, multi_wave_hist_algor, mixed_psd_prop, mixed_p2v_prop, mixed_hist_algor)
 
-    plot_psd_algo(wave_psd_prop, pulse_psd_prop, wave_p2v_prop, pulse_p2v_prop, wave_skewness, pulse_skewness)
+    plot_psd_algo(wave_psd_prop, pulse_psd_prop, multi_wave_psd_prop)
 
-    plot_p2v_algo(wave_psd_prop, pulse_psd_prop, wave_p2v_prop, pulse_p2v_prop)
+    plot_p2v_algo(wave_p2v_prop, pulse_p2v_prop, multi_wave_p2v_prop)
+
+    plot_hist_algo(wave_hist_algor, pulse_hist_algor, multi_wave_hist_algor)
 
     plt.show()
 
