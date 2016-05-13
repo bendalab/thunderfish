8cfe0602b551fd48d5a291954320694748a36fe5
Jan Benda <jan.benda@uni-tuebingen.de>
2016-05-13 23:37:42 +0200

    added playaudio module

--- thunderfish/fishfinder.py
+++ thunderfish/fishfinder.py
@@ -12,7 +12,7 @@ import configfile as cf
 import dataloader as dl
 import harmonicgroups as hg
 import bestwindow as bw
-import pyaudio
+import playaudio as pa
 
 # check: import logging https://docs.python.org/2/howto/logging.html#logging-basic-tutorial
 
@@ -68,85 +68,6 @@ cfg.add_section('Debugging:')
 cfg.add('verboseLevel', 0, '', '0=off upto 4 very detailed')
 
 
-###############################################################################
-## audio output:
-
-def init_audio() :
-    """
-    Initializes audio output.
-
-    Returns:
-        audio: a handle for subsequent calls of play() and close_audio()
-    """
-    oldstderr = os.dup( 2 )
-    os.close( 2 )
-    tmpfile = 'tmpfile.tmp'
-    os.open( tmpfile, os.O_WRONLY | os.O_CREAT )
-    audio = pyaudio.PyAudio()
-    os.close( 2 )
-    os.dup( oldstderr )
-    os.close( oldstderr )
-    os.remove( tmpfile )
-    return audio
-
-def close_audio( audio ) :
-    """
-    Close audio output.
-
-    Args:
-        audio: the handle returned by init_audio()
-    """
-    audio.terminate()           
-
-def play_audio( audio, data, rate ) :
-    """
-    Play audio data.
-
-    Args:
-        audio: the handle returned by init_audio()
-        data (array): the data to be played
-        rate (float): the sampling rate in Hertz
-    """
-    # print 'play'
-    stream = audio.open( format=pyaudio.paInt16, channels=1, rate=rate, output=True )
-    rawdata = data - np.mean( data )
-    rawdata /= np.max( rawdata )*2.0
-    ## nr = int( np.round( 0.1*rate ) )
-    ## if len( rawdata ) > 2*nr :
-    ##     for k in xrange( nr ) :
-    ##         rawdata[k] *= float(k)/nr
-    ##         rawdata[len(rawdata)-k-1] *= float(k)/nr
-    # somehow more than twice as many data are needed:
-    rawdata = np.hstack( ( rawdata, np.zeros( 11*len( rawdata )/10 ) ) )
-    ad = np.array( np.round(2.0**15*rawdata) ).astype( 'i2' )
-    stream.write( ad )
-    stream.stop_stream()
-    stream.close()
-
-def play_tone( audio, frequency, duration, rate ) :
-    """
-    Play a tone of a given frequency and duration.
-
-    Args:
-        audio: the handle returned by init_audio()
-        frequency (float): the frequency of the tone in Hertz
-        duration (float): the duration of the tone in seconds
-        rate (float): the sampling rate in Hertz
-    """
-    stream = audio.open( format=pyaudio.paInt16, channels=1, rate=rate, output=True )
-    time = np.arange( 0.0, duration, 1.0/rate )
-    data = np.sin(2.0*np.pi*frequency*time)
-    nr = int( np.round( 0.1*rate ) )
-    for k in xrange( nr ) :
-        data[k] *= float(k)/float(nr)
-        data[len(data)-k-1] *= float(k)/float(nr)
-    ## somehow more than twice as many data are needed:
-    data = np.hstack( ( data, np.zeros( 11*len( data )/10 ) ) )
-    ad = np.array( np.round(2.0**14*data) ).astype( 'i2' )
-    stream.write( ad )
-    stream.stop_stream()
-    stream.close()
-
     
 ###############################################################################
 ## plotting etc.
@@ -193,7 +114,7 @@ class SignalPlot :
         self.generate_color_range()
 
         # audio output:
-        self.audio = init_audio()
+        self.audio = pa.open()
 
         # set key bindings:
         plt.rcParams['keymap.fullscreen'] = 'ctrl+f'
@@ -262,7 +183,7 @@ class SignalPlot :
         plt.show()
 
     def __del( self ) :
-        close_audio( self.audio )
+        pa.close(self.audio)
 
     def generate_color_range( self ) :
          # color and marker range:
@@ -803,16 +724,16 @@ class SignalPlot :
         plt.close( fig )
         print 'saved waveform figure to', figfile
 
-    def play_segment( self ) :
+    def play_segment(self) :
         t0 = int(np.round(self.toffset*self.rate))
         t1 = int(np.round((self.toffset+self.twindow)*self.rate))
-        play_audio( self.audio, self.data[t0:t1], self.rate )
+        pa.play(self.audio, self.data[t0:t1], self.rate)
         
-    def play_all( self ) :
-        play_audio( self.audio, self.data, self.rate )
+    def play_all(self) :
+        pa.play(self.audio, self.data, self.rate)
         
     def play_tone( self, frequency ) :
-        play_tone( self.audio, frequency, 1.0, self.rate )
+        pa.play_tone(self.audio, frequency, 1.0, self.rate)
 
                             
 def short_user_warning(message, category, filename, lineno, file=sys.stderr, line=''):
