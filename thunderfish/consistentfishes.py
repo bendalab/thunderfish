16483aa9d60062be22b1b36f7d4d714ac3d8c8e3
Till Raab <till.raab@student.uni-tuebingen.de>
2016-05-31 14:41:19 +0200

    created the function find_consistancy. This function gets several lists with floats and compares them. When a value of the first list is in all the other lists (+-threshhold) it becomes parte of the output list of the function. Furthermore the index of this value in the first input list is given back in the second output list. The function consistant_fishlist() gets the initial fishlists and the indeces of the consistant fishes to create a new fishlist witch only contains these fishes that are consistant in all input fishlists. Last I added an artificial fishlist to test the code

--- thunderfish/consistentfishes.py
+++ thunderfish/consistentfishes.py
@@ -5,24 +5,50 @@ def extract_fundamentals(fishlists):
     for fishlist in np.arange(len(fishlists)):
         for fish in np.arange(len(fishlists[fishlist])):
             fundamentals[fishlist].append(fishlists[fishlist][fish][0][0])
+
     return fundamentals
 
 def find_consistency(fundamentals):
-    consistent_fundamentals = []
+    consistancy_help = [1 for i in np.arange(len(fundamentals[0]))]
     index = []
+    consistent_fundamentals = []
+
+    for enu, fundamental in enumerate(fundamentals[0]):
+        for list in np.arange(len(fundamentals)-1)+1:
+            freq_diff = [fundamentals[list][i] - fundamental for i in np.arange(len(fundamentals[list]))]
+            for i in np.arange(len(freq_diff)):
+                if freq_diff[i] >= -1.0 and freq_diff[i] <= 1:
+                    consistancy_help[enu] += 1
+                    break
+
+    for idx in np.arange(len(consistancy_help)):
+        if consistancy_help[idx] == len(fundamentals):
+            index.append(idx)
+            consistent_fundamentals.append(fundamentals[0][idx])
+
     return consistent_fundamentals, index
 
 def consistent_fishlist(index, fishlists):
-    return consistent_fishlist
+    filtered_fishlist = []
+    for idx in index:
+        filtered_fishlist.append(fishlists[0][idx])
+
+    return filtered_fishlist
 
 def consistentfishes_main(fishlists):
     print('comparing different fishlists ...')
+
     fundamentals = extract_fundamentals(fishlists)
     consistant_fundamentals, index = find_consistency(fundamentals)
     filtered_fishlist = consistent_fishlist(index, fishlists)
+
     return filtered_fishlist
 
 if __name__ == '__main__':
 
-    fishlists = []
+    fishlists = [ [[[350, 0]], [[700.2, 0]], [[1000, 0]]],
+                  [[[350.1, 0]], [[699.8, 0]], [[250.2, 0]]],
+                  [[[349.7, 0]], [[700.4, 0]], [[1000.2, 0]]] ]
     filtered_fishlist = consistentfishes_main(fishlists)
+
+    print filtered_fishlist
