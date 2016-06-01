33afdd26e79c79fcf1a485dfe8b202baf047308a
Till Raab <till.raab@student.uni-tuebingen.de>
2016-06-01 13:36:38 +0200

    little changes: extracted the calculation from the proportioins out of a funtion and renamed a function.

--- thunderfish/psdtype.py
+++ thunderfish/psdtype.py
@@ -8,7 +8,7 @@ def bin_it(power, freq_bins, max_freq, res):
         power_db.append(tmp_power_db)
     return power_db
 
-def get_proportions(power_db, return_all=False):
+def get_bin_percentiles(power_db):
     power_db_top = np.ones(len(power_db))
     power_db_upper_middle = np.ones(len(power_db))
     power_db_lower_middle = np.ones(len(power_db))
@@ -20,31 +20,26 @@ def get_proportions(power_db, return_all=False):
         power_db_lower_middle[fbin] = np.percentile(power_db[fbin], 25)
         power_db_bottom[fbin] = np.percentile(power_db[fbin], 1)
 
-    proportions = [(power_db_upper_middle[i] - power_db_lower_middle[i]) / (power_db_top[i] - power_db_bottom[i])
-                   for i in np.arange(len(power_db))]
+    return [power_db_top, power_db_upper_middle, power_db_lower_middle, power_db_bottom]
 
-    if return_all:
-        return proportions, [power_db_top, power_db_upper_middle, power_db_lower_middle, power_db_bottom]
-    else:
-        return proportions
 
-def psd_type_main(power, freqs, freq_bins=125, max_freq = 3000, return_all= False):
+def psd_type_main(power, freqs, freq_bins=125, max_freq = 3000, return_percentiles= False):
     print('try to figure out psd type ...')
-    res = freqs[-1]/len(freqs)
+    res = freqs[-1]/len(freqs) # resolution
 
     power_db = bin_it(power, freq_bins, max_freq, res)
 
-    if return_all:
-        proportions, percentiles= get_proportions(power_db, return_all)
-    else:
-        proportions= get_proportions(power_db)
+    percentiles= get_bin_percentiles(power_db)
+
+    proportions = [(percentiles[1][i] - percentiles[2][i]) / (percentiles[0][i] - percentiles[3][i])
+                   for i in np.arange(len(power_db))]
 
     if np.mean(proportions) < 0.27:
         psd_type = 'wave'
     else:
         psd_type = 'pulse'
 
-    if return_all:
+    if return_percentiles:
         return psd_type, proportions, percentiles
     else:
         return psd_type, proportions
@@ -64,6 +59,7 @@ def get_example_data(audio_file, channel=0, verbose=None):
     print('calculating best window ...\n')
     bwin_data, clip = bw.best_window(data, samplrate)
 
+    print('calculation powerspecturm ...\n')
     power, freqs = ps.powerspectrum_main(bwin_data, samplrate)
 
     return power, freqs
@@ -90,8 +86,8 @@ if __name__ == '__main__':
     file = sys.argv[-1]
     power, freqs = get_example_data(file)
 
-    psd_type, proportions, percentiles = psd_type_main(power, freqs, return_all=True)
-    print ('The PSD belongs to a %s-fish (%.2f)' %(psd_type, np.mean(proportions)))
+    psd_type, proportions, percentiles = psd_type_main(power, freqs, return_percentiles=True)
+    print ('The PSD belongs to a %s-fish (%.2f)' %(psd_type, float(np.mean(proportions))))
 
     if plot:
         import matplotlib.pyplot as plt
@@ -106,5 +102,7 @@ if __name__ == '__main__':
                             alpha=0.7)
             ax.fill_between([bin * 125, (bin + 1) * 125], percentiles[2][bin], percentiles[3][bin], color='red',
                             alpha=0.7)
-
+        ax.set_xlim([0, 3000])
+        ax.set_xlabel('Frequency')
+        ax.set_ylabel('Power [dB]')
         plt.show()
