dad1f4e52ea7581e762be4851e494d1dee3db041
Till Raab <till.raab@student.uni-tuebingen.de>
2016-06-06 14:10:32 +0200

    A new function, powerspectrumplot, has been added to the modul. This function can together with some kwargs be given to the powerspectrum_main function. The purpose of this function is to ceate an ais for plotting the powerspectrum and returning it as an extra output of the main function.

--- thunderfish/powerspectrum.py
+++ thunderfish/powerspectrum.py
@@ -27,7 +27,22 @@ def calc_psd(data, samplingrate, nfft):
     power, freqs = ml.psd(data, NFFT=nfft, noverlap=nfft / 2, Fs=samplingrate, detrend=ml.detrend_mean)
     return power, freqs
 
-def powerspectrum_main(data, samplingrate, fresolution=0.5):
+def powerspectrumplot(power, freqs, ax):
+    """
+    Plots a powerspectum.
+
+    :param power:               (1-D array) power array of a psd.
+    :param freqs:               (1-D array) frequency array of a psd.
+    :param ax:                  (axis for plot) empty axis that is filled with content in the function.
+    :return ax:                 (axis for plot) axis that is ready for plotting containing the powerspectrum.
+    """
+    ax.plot(freqs, power)
+    ax.set_ylabel('power')
+    ax.set_xlabel('frequency [Hz]')
+    ax.set_xlim([0, 3000])
+    return ax
+
+def powerspectrum_main(data, samplingrate, fresolution=0.5, plot_data_func=None, **kwargs):
     """
     This function is performing the steps to calculate a powerspectrum on the basis of a given dataset, a given
     samplingrate and a given frequencyresolution for the psd. Therefore two other functions are called to first
@@ -36,8 +51,11 @@ def powerspectrum_main(data, samplingrate, fresolution=0.5):
     :param data:                (1-D array) data array you want to calculate a psd of.
     :param samplingrate:        (float) sampling rate of the data that you want to calculate a psd of.
     :param fresolution:         (float) frequency resolution of the psd
+    :param plot_data_func:      (function) function (powerspectrumplot()) that is used to create a axis for later plotting containing the calculated powerspectrum.
+    :param **kwargs:            additional arguments that are passed to the plot_data_func().
     :return power:              (1-D array) power array of the psd.
     :return freqs:              (1-D array) psd array of the psd.
+    :return ax:                 (axis for plot) axis that is ready for plotting containing a figure that shows what the modul did.
     """
 
     # print("calculating powerspecturm ...")
@@ -45,35 +63,27 @@ def powerspectrum_main(data, samplingrate, fresolution=0.5):
     nfft = calc_nfft(samplingrate, fresolution)
     power, freqs = calc_psd(data, samplingrate, nfft)
 
-    return power, freqs
+    if plot_data_func:
+        ax = plot_data_func(power, freqs, **kwargs)
+        return power, freqs, ax
+    else:
+        return power, freqs
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
     import sys
 
     print('Computes powerspectrum of a created signal of two wavefish (300 and 450 Hz)')
     print('')
     print('Usage:')
-    print('  python powerspectrum.py [-p]')
-    print('  -p: plot data')
+    print('  python powerspectrum.py')
     print('')
-
-    plot = False
-    if len(sys.argv) > 1 and sys.argv[1] == '-p':
-        plot = True
 
     fundamental = [300, 450] # Hz
     samplingrate = 100000
     time = np.linspace(0, 8-1/samplingrate, 8*samplingrate)
     data = np.sin(time * 2 * np.pi* fundamental[0]) + np.sin(time * 2 * np.pi* fundamental[1])
 
-    power, freqs = powerspectrum_main(data, samplingrate)
-
-    if plot:
-        print('plotting example psd ...')
-        fig, ax = plt.subplots()
-        ax.plot(freqs, power)
-        ax.set_ylabel('power')
-        ax.set_xlabel('frequency [Hz]')
-        ax.set_xlim([0, 3000])
-        plt.show()
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots()
+    power, freqs, ax = powerspectrum_main(data, samplingrate, plot_data_func=powerspectrumplot, ax=ax)
+    plt.show()
