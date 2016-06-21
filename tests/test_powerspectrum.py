e4926f9216a60ca872c002a83859950c4177dd5c
Till Raab <till.raab@student.uni-tuebingen.de>
2016-06-21 15:29:31 +0200

    Test for powerspectrum modul is working. This Test is creating an artificial sinus with a frequency of 300 Hz. With this the powerspectrum modul is tested. One time with 2 frequency resolutions and one time with two frequency resolutions. For the comparison the rounded peak frequency is compared with the frequency used for the creation of the sinus.

--- tests/test_powerspectrum.py
+++ tests/test_powerspectrum.py
@@ -1,20 +1,25 @@
 from nose.tools import assert_true, assert_equal, assert_almost_equal
 import numpy as np
-import thunderfish.thunderfish.powerspectrum as ps
+import thunderfish.powerspectrum as ps
 
 def test_powerspectrum():
     # generate data
     fundamental = 300.  # Hz
     samplerate = 100000
     time = np.linspace(0, 8 - 1 / samplerate, 8 * samplerate)
-    data = np.sin(time * 2 * np.pi * fundamental[0]) + np.sin(time * 2 * np.pi * fundamental[1])
+    data = np.sin(time * 2 * np.pi * fundamental)
 
+    # run multi_resolution_psd with 2 fresolutions (list)
     psd_data = ps.multi_resolution_psd(data, samplerate, fresolution=[0.5, 1], verbose=1)
 
-    assert_equal(psd_data[0][1][psd_data[0][0].argsort()[-1]], fundamental, 'peak in PSD is not the fundamental '
-                                                                            'frequency given.')
-
+    # test the results
+    assert_equal(round(psd_data[0][1][psd_data[0][0].argsort()[-1]]), fundamental, 'peak in PSD is not the fundamental '
+                                                                                   'frequency given.')
+    assert_equal(round(psd_data[1][1][psd_data[1][0].argsort()[-1]]), fundamental, 'peak in PSD is not the fundamental '
+                                                                                   'frequency given.')
+    # run multi_resolution_psd with 1 fresolutions (float)
     psd_data = ps.multi_resolution_psd(data, samplerate, fresolution=0.5, verbose=0)
 
-    assert_equal(psd_data[1][psd_data[0].argsort()[-1]], fundamental, 'peak in PSD is not the fundamental frequency'
-                                                                      ' given.')
+    # test the result
+    assert_equal(round(psd_data[1][psd_data[0].argsort()[-1]]), fundamental, 'peak in PSD is not the fundamental '
+                                                                             'frequency given.')
