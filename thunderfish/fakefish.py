ad28191c0257a20b3ae754489e8333e066773f19
Sehuanes <jfsehuanes@gmail.com>
2016-06-29 17:14:26 +0200

    almost done. Need to finish plot for pulsefish.

--- thunderfish/fakefish.py
+++ thunderfish/fakefish.py
@@ -3,9 +3,18 @@ import matplotlib.pyplot as plt
 from IPython import embed
 
 
-def generate_wavefish(freq, samplerate, time_len=20., harmonics=5):
+def generate_wavefish(freq, samplerate, time_len=20., harmonics=3):
 
     # generate time
+    """
+
+    :param freq: (float). Frequency of the fish in Hz.
+    :param samplerate: (float). Sampling rate in Hz.
+    :param time_len: (float). Length of the recording in sec.
+    :param harmonics: (int). Number of harmonics of freq to be included.
+
+    :return: (array). Data of a wavefish with given frequency.
+    """
     time = np.arange(0, time_len, 1./samplerate)
 
     data = np.sin(2 * np.pi * time * freq)
@@ -16,11 +25,7 @@ def generate_wavefish(freq, samplerate, time_len=20., harmonics=5):
     # Insert some noise
     data += 0.05 * np.random.randn(len(data))
 
-    fig, ax = plt.subplots()
-    ax.plot(time, data, lw=2.5)
-    plt.show()
-
-    pass
+    return data
 
 
 def generate_pulsefish(freq, samplerate, time_len=20., noise_fac=0.1, wave_cut=0.6,
@@ -40,6 +45,7 @@ def generate_pulsefish(freq, samplerate, time_len=20., noise_fac=0.1, wave_cut=0
     :param tr_len: (float). length of the negative part of the pulse.
     :param tr_std: (float). std of the negative part of the pulse.
     :param tr_fac: (float). Factor for regulating the negative part of the pulse.
+
     :return: (array). Data with pulses at the given frequency.
     """
 
@@ -117,6 +123,36 @@ def generate_pulsefish(freq, samplerate, time_len=20., noise_fac=0.1, wave_cut=0
 if __name__ == '__main__':
 
     samplerate = 20000.  # in Hz
+    fs = 16.  # Font size
+    rec_length = 20.  # in sec
+    inset_len = 0.02  # in sec
 
-    # generate_pulsefish(80., samplerate)
-    wavefish = generate_wavefish(300., samplerate)
+    time = np.arange(0, rec_length, 1./samplerate)
+
+    pulsefish = generate_pulsefish(80., samplerate, time_len=rec_length,
+                                   noise_fac=0.1, wave_cut=0.6,
+                                   pk_len=0.001, pk_std=0.1, peak_fac=2.,
+                                   tr_len=0.001, tr_std=0.12, tr_fac=1.2)
+
+    wavefish = generate_wavefish(300., samplerate, time_len=rec_length, harmonics=3)
+
+    fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(19, 10))
+
+    # embed()
+    # quit()
+
+    # ax[0] is complete wavefish
+    ax[0][0].plot(time, wavefish, color='dodgerblue', alpha=0.7, lw=2)
+    ax[0][0].set_title('Fake-wavefish-RECORDING', fontsize=fs+2)
+
+    # ax[1] is wavefish inset
+    ax[0][1].plot(time[:samplerate*inset_len], wavefish[:samplerate*inset_len], '-o',
+                  lw=3, color='dodgerblue', ms=10, mec='k', mew=1.5)
+    ax[0][1].set_title('Fake-wavefish-INSET', fontsize=fs + 2)
+
+    for wave_ax in [ax[0][0], ax[0][1]]:
+        wave_ax.set_xlabel('Time [sec]', fontsize=fs)
+        wave_ax.set_ylabel('Amplitude [a.u.]', fontsize=fs)
+        wave_ax.tick_params(axis='both', which='major', labelsize=fs - 2)
+
+    plt.show()
