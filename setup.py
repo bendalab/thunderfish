eb7fe2335f3e92f690d9458b85d34f8341dc1838
Jan Benda <jan.benda@uni-tuebingen.de>
2016-07-02 11:13:47 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='thunderfish',
-      version='0.5',
+      version='0.5.0',     # see http://semver.org/
       packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
-      description='Python scripts for analyzing recordings of e-fish electric fields.',
-      author='Juan F. Sehuanes, Till Raab, Fabian Sinz, Jan Benda, Joerg Henninger, Jan Grewe',
-      requires=['numpy', 'audioio', 'matplotlib']
+      description='Algorithms and scripts for analyzing recordings of e-fish electric fields.',
+      author='Jan Benda, Juan F. Sehuanes, Till Raab, Joerg Henninger, Jan Grewe, Fabian Sinz',
+      requires=['numpy', 'matplotlib', 'audioio']
       )
