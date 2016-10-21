7434babec1ea44d3817065017f760f7f3f989b3b
Jan Benda <jan.benda@uni-tuebingen.de>
2016-10-21 23:21:09 +0200

    added test for fakefish

--- tests/test_fakefish.py
+++ tests/test_fakefish.py
@@ -7,17 +7,60 @@ def test_harmonic_groups():
 
     # generate data:
     samplerate = 44100.0
-    df = 0.5
-    eodfs = np.array([123.0, 321.0, 666.0, 668.0])
-    fish1 = ff.generate_wavefish(eodfs[0], samplerate, duration=8.0, noise_std=0.01,
-                                 amplitudes=[1.0, 0.5, 0.2, 0.1, 0.05])
-    fish2 = ff.generate_wavefish(eodfs[1], samplerate, duration=8.0, noise_std=0.01,
-                                 amplitudes=[1.0, 0.7, 0.2, 0.1])
-    fish3 = ff.generate_wavefish(eodfs[2], samplerate, duration=8.0, noise_std=0.01,
-                                 amplitudes=[10.0, 5.0, 1.0])
-    fish4 = ff.generate_wavefish(eodfs[3], samplerate, duration=8.0, noise_std=0.01,
-                                 amplitudes=[6.0, 3.0, 1.0])
+    duration = 1.
+    
+    # generate data:
+    time = np.arange(0, duration, 1./samplerate)
 
-    # check:
-    assert_true(np.all(np.abs(eodfs-fundamentals) < df),
-                'harmonic_groups() did not correctly detect all fundamental frequencies')
+    # wavefish with fixed frequency:
+    eodf = 300.0
+    data = ff.generate_wavefish(eodf, samplerate, duration=duration, noise_std=0.02, 
+                                amplitudes=[1.0, 0.5, 0.0, 0.0001],
+                                phases=[0.0, 0.0, 0.0, 0.0])
+    assert_equal(len(time), len(data), 'generate_wavefish() failed')
+
+    data = ff.generate_alepto(eodf, samplerate, duration=duration)
+    assert_equal(len(time), len(data), 'generate_alepto() failed')
+
+    data = ff.generate_eigenmannia(eodf, samplerate, duration=duration)
+    assert_equal(len(time), len(data), 'generate_eigenmannia() failed')
+    
+    
+    # wavefish with frequency modulation:
+    eodf = 500.0 - time/duration*400.0
+    data = ff.generate_wavefish(eodf, samplerate, duration=duration, noise_std=0.02, 
+                                amplitudes=[1.0, 0.5, 0.0, 0.0001],
+                                phases=[0.0, 0.0, 0.0, 0.0])
+    assert_equal(len(time), len(data), 'generate_wavefish() failed')
+
+    data = ff.generate_alepto(eodf, samplerate, duration=duration)
+    assert_equal(len(time), len(data), 'generate_alepto() failed')
+
+    data = ff.generate_eigenmannia(eodf, samplerate, duration=duration)
+    assert_equal(len(time), len(data), 'generate_eigenmannia() failed')
+
+    # pulse fishes:
+    data = ff.generate_pulsefish(80., samplerate, duration=duration,
+                                 noise_std=0.02, jitter_cv=0.1,
+                                 peak_stds=[0.0001, 0.0002],
+                                 peak_amplitudes=[1.0, -0.3],
+                                 peak_times=[0.0, 0.0003])
+    assert_equal(len(time), len(data), 'generate_pulsefish() failed')
+    
+    data = ff.generate_monophasic_pulses(80., samplerate, duration=duration)
+    assert_equal(len(time), len(data), 'generate_monophasic_pulsefish() failed')
+
+    data = ff.generate_biphasic_pulses(80., samplerate, duration=duration)
+    assert_equal(len(time), len(data), 'generate_biphasic_pulsefish() failed')
+
+    data = ff.generate_triphasic_pulses(80., samplerate, duration=duration)
+    assert_equal(len(time), len(data), 'generate_triphasic_pulsefish() failed')
+
+    # communication signals:
+    data = ff.chirps_frequency(600.0, samplerate, duration=duration, chirp_kurtosis=1.0)
+    assert_equal(len(time), len(data), 'chirps_frequency() failed')
+
+    data = ff.rises_frequency(600.0, samplerate, duration=duration, rise_size=20.0)
+    assert_equal(len(time), len(data), 'rises_frequency() failed')
+    
+    
