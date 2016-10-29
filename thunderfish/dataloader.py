ec93573defa8cc50d9fc5ad639088ee7d5e1ba4f
Jan Benda <jan.benda@uni-tuebingen.de>
2016-10-29 12:05:43 +0200

    added relacs file load functions

--- thunderfish/dataloader.py
+++ thunderfish/dataloader.py
@@ -1,6 +1,215 @@
 import os.path
+import glob
 import numpy as np
 import audioio as aio
+
+
+def relacs_samplerate_unit(filename, channel=0):
+    """
+    Opens the corresponding stimuli.dat file and reads the sampling rate and unit.
+
+    Parameters
+    ----------
+    filename: string
+        path to a relacs data directory, a file in a relacs data directory,
+        or a relacs trace-*.raw files.
+    channel: int
+        the channel (trace) number, if filename does not specify a trace-*.raw file.
+
+    Returns
+    -------
+    samplerate: float
+        the sampling rate in Hertz
+    unit: string
+        the unit of the trace, can be empty if not found
+
+    Raises
+    ------
+    IOError/FileNotFoundError:
+        If the stimuli.dat file does not exist.
+    ValueError:
+        stimuli.dat file does not contain sampling rate.
+    """
+
+    trace = channel+1
+    relacs_dir = filename
+    # check for relacs data directory:
+    if not os.path.isdir(filename):
+        relacs_dir = os.path.dirname(filename)
+        bn = os.path.basename(filename)
+        if (len(bn) > 5 and bn[0:5] == 'trace' and bn[-4:] == '.raw'):
+            trace = int(bn[6:].replace('.raw', ''))
+
+    # retreive sampling rate and unit from stimuli.dat file:
+    samplerate = None
+    unit = ""
+    stimuli_file = os.path.join(relacs_dir, 'stimuli.dat')
+    with open(stimuli_file, 'r') as sf:
+        for line in sf:
+            if len(line) == 0 or line[0] != '#':
+                break
+            if "unit%d" % trace in line:
+                unit = line.split(':')[1].strip()
+            if "sampling rate%d" % trace in line:
+                value = line.split(':')[1].strip()
+                samplerate = float(value.replace('Hz',''))
+
+    if samplerate is not None:
+        return samplerate, unit
+    raise ValueError('could not retrieve sampling rate from ' + stimuli_file)
+
+
+def relacs_metadata(filename):
+    """
+    Reads header of a relacs *.dat file.
+
+    Parameters
+    ----------
+    filename: string
+        a relacs *.dat file.
+
+    Returns
+    -------
+    data: dict
+        dictionary with the content of the file header.
+        
+    Raises
+    ------
+    IOError/FileNotFoundError:
+        If filename cannot be opened.
+    """
+    
+    data = {}
+    with open(filename, 'r') as sf:
+        for line in sf:
+            if len(line) == 0 or line[0] != '#':
+                break
+            words = line.split(':')
+            if len(words) >= 2:
+                key = words[0].strip('# ')
+                value = ':'.join(words[1:]).strip()
+                data[key] = value
+    return data
+
+
+def check_relacs(filepathes):
+    """
+    Check whether filepathes are relacs files.
+
+    Parameters
+    ----------
+    filepathes: string or list of strings
+        path to a relacs data directory, a file in a relacs data directory,
+        or relacs trace-*.raw files.
+
+    Returns
+    -------
+    If filepathes is a single path, then returns True if it is a file in
+    a valid relacs data directory.
+    If filepathes are more than one path, then returns True if filepathes
+    are trace-*.raw files in a valid relacs data directory.
+    """
+
+    path = filepathes
+    # filepathes must be trace-*.raw:
+    if type(filepathes) is list:
+        if len(filepathes) > 1:
+            for file in filepathes:
+                bn = os.path.basename(file)
+                if len(bn) <= 5 or bn[0:5] != 'trace' or bn[-4:] != '.raw':
+                    return False
+        path = filepathes[0]
+    # relacs data directory:
+    relacs_dir = path
+    if not os.path.isdir(path):
+        relacs_dir = os.path.dirname(path)
+    # check for a valid relacs data directory:
+    if (os.path.isfile(os.path.join(relacs_dir, 'stimuli.dat')) and
+        os.path.isfile(os.path.join(relacs_dir, 'trace-1.raw'))):
+        return True
+    else:
+        return False
+    
+
+def load_relacs(filepathes, verbose=0):
+    """
+    Load traces (trace-*.raw files) that have been recorded with relacs (www.relacs.net).
+
+    Parameters
+    ----------
+    filepathes: string or list of string
+        path to a relacs data directory, a relacs stimuli.dat file, a relacs info.dat file,
+        or relacs trace-*.raw files.
+
+    Returns
+    -------
+    data: 2-D array
+        the data, first dimension time, second dimension channel
+    samplerate: float
+        the sampling rate of the data in Hz
+    unit: string
+        the unit of the data
+
+    Raises
+    ------
+    ValueError:
+        - Invalid name for relacs trace-*.raw file.
+        - Sampling rates of traces differ.
+        - Unit of traces differ.
+    """
+    
+    # fix pathes:
+    if type(filepathes) is not list:
+        filepathes = [filepathes]
+    if len(filepathes) == 1:
+        if os.path.isdir(filepathes[0]):
+            filepathes = glob.glob(os.path.join(filepathes[0], 'trace-*.raw'))
+        else:
+            bn = os.path.basename(filepathes[0])
+            if len(bn) <= 5 or bn[0:5] != 'trace' or bn[-4:] != '.raw':
+                filepathes = glob.glob(os.path.join(os.path.dirname(filepathes[0]),
+                                                    'trace-*.raw'))
+                
+    # load trace*.raw files:
+    nchannels = len(filepathes)
+    data = None
+    nrows = 0
+    samplerate = None
+    unit = ""
+    for n, path in enumerate(filepathes):
+        bn = os.path.basename(path)
+        if len(bn) <= 5 or bn[0:5] != 'trace' or bn[-4:] != '.raw':
+            raise ValueError('invalid name %s of relacs trace file', path)
+        x = np.fromfile(path, np.float32)
+        if verbose > 0:
+            print( 'loaded %s' % path)
+        if data is None:
+            nrows = len(x)-2
+            data = np.empty((nrows, nchannels))
+        data[:,n] = x[:nrows]
+        # retrieve sampling rate and unit:
+        rate, us = relacs_samplerate_unit(path)
+        if samplerate is None:
+            samplerate = rate
+        elif rate != samplerate:
+            raise ValueError('sampling rates of traces differ')
+        if len(unit) == 0:
+            unit = us
+        elif us != unit:
+            raise ValueError('unit of traces differ')
+    return data, samplerate, unit
+
+
+def check_pickle(filepath):
+    """
+    Check if filepath is a pickle file.
+    
+    Returns
+    -------
+    True, if fielpath is a pickle file.
+    """
+    ext = os.path.splitext(filepath)[1]
+    return ext == 'pkl'
 
 
 def load_pickle(filename, channel=0):
@@ -33,36 +242,49 @@ def load_data(filepath, channel=0, verbose=0):
     """
     Call this function to load a single trace of data from a file.
 
-    Args:
-        filepath (string): the full path and name of the file to load
-        channel (int): the single channel to be returned
-        verbose (int): if >0 show detailed error/warning messages
+    Parameters
+    ----------
+    filepath: string
+        the full path and name of the file to load
+    channel: int
+        the single channel to be returned
+    verbose: int
+        if >0 show detailed error/warning messages
 
-    Returns:
-        data (array): the data trace as a 1-D numpy array
-        samplerate (float): the sampling rate of the data in Hz
-        unit (string): the unit of the data
+    Returns
+    -------
+    data: array
+        the data trace as a 1-D numpy array
+    samplerate: float
+        the sampling rate of the data in Hz
+    unit: string
+        the unit of the data
+
+    Raise
+    -----
+    ValueError:
+        input argument filepath is empty string or list.
     """
+    
     # check values:
     data = np.array([])
     samplerate = 0.0
     unit = ''
     if len(filepath) == 0:
-        print('load_data(): input argument filepath is empty string!')
-        return data, samplerate, unit
-    if not os.path.isfile(filepath):
-        print('load_data(): input argument filepath=%s does not indicate an existing file!' % filepath)
-        return data, samplerate, unit
-    if os.path.getsize(filepath) <= 0:
-        print('load_data(): input argument filepath=%s indicates file of size 0!' % filepath)
-        return data, samplerate, unit
-    if channel < 0:
-        print('load_data(): input argument channel=%d is negative!' % channel)
-        channel = 0
+        raise ValueError('input argument filepath is empty string or list.')
 
     # load data:
-    ext = filepath.split('.')[-1]
-    if ext == 'pkl':
+    if check_relacs(filepath):
+        data, samplerate, unit = load_relacs(filepath, verbose)
+        channels = data.shape[1]
+        if channel >= channels:
+            print('number of channels in file %s is %d, but requested channel %d' %
+                  (filepath, channels, channel))
+            channel = channels - 1
+        if channel >= 0:
+            data = data[:, channel]
+        return data, samplerate, unit
+    elif check_pickle(filepath):
         data, samplerate, unit = load_pickle(filepath, channel)
     else:
         data, samplerate = aio.load_audio(filepath, verbose)
@@ -133,6 +355,11 @@ open_data = DataLoader
 if __name__ == "__main__":
     import sys
     import matplotlib.pyplot as plt
+    
+    data, rate, unit = load_relacs(sys.argv[1:])
+    print(data, rate, unit)
+    exit()
+
 
     print("Checking dataloader module ...")
     print('')
