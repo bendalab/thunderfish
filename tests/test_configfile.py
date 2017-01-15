07590834f7b3145220fbe5cfa3a71194a8de8f13
Jan Benda <jan.benda@uni-tuebingen.de>
2017-01-15 11:30:00 +0100

    fixed configuration test

--- tests/test_configfile.py
+++ tests/test_configfile.py
@@ -1,4 +1,5 @@
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_not_equal
+from copy import deepcopy
 import thunderfish.configfile as cf
 import thunderfish.bestwindow as bw
 from collections import OrderedDict
@@ -11,24 +12,34 @@ def test_config_file():
     bw.add_best_window_config(cfg)
 
     cfgfile = 'test.cfg'
-
-    # write configuration to a file:
-    cfg.dump(cfgfile, 'header', 50)
+    cfgdifffile = 'testdiff.cfg'
 
     # manipulate some values:
-    cfg2 = cf.ConfigFile(cfg)
+    cfg2 = deepcopy(cfg)
     cfg2.set('bestWindowSize', 100.0)
     cfg2.set('weightCVAmplitude', 20.0)
     cfg2.set('clipBins', 300)
-    cfg3 = cf.ConfigFile(cfg2)
+    cfg3 = deepcopy(cfg2)
+
+    # write configurations to files:
+    cfg.dump(cfgfile, 'header', maxline=50)
+    cfg2.dump(cfgdifffile, diff_only=True)
+
+    # test modified configuration:
+    assert_not_equal(cfg, cfg2, 'cfg and cfg2 should differ')
 
     # read it in:
     cfg2.load(cfgfile)
-    assert_equal(cfg, cfg2)
+    assert_equal(cfg, cfg2, 'cfg and cfg2 should be the same')
+
+    # read manipulated values:
+    cfg2.load(cfgdifffile)
+    assert_equal(cfg2, cfg3, 'cfg2 and cfg3 should be the same')
 
     # read it in:
     cfg3.load_files(cfgfile, 'data.dat')
-    assert_equal(cfg, cfg3)
+    assert_equal(cfg, cfg3, 'cfg and cfg3 should be the same')
 
     # clean up:
     os.remove(cfgfile)
+    os.remove(cfgdifffile)
